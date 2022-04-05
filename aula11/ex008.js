7a0a7b8281090f22b7c30a5d51c04c39c3d39489
erickbarauna <ericksbarauna26@gmail.com>
2022-04-05 10:17:04 -0300

    atualizado

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {  // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
