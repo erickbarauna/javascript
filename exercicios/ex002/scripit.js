d3c884061d6e52210b863968691af934356cd53a
erickbarauna <ericksbarauna26@gmail.com>
2022-04-11 08:58:54 -0300

    atualizado

---
+++ exercicios/ex002/scripit.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - fano.value
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = 
+    }
+}
