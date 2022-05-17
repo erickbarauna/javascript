b7221698d628248c0bc52733eae4c89328b17df1
Erick Barauna <98670029+erickbarauna@users.noreply.github.com>
2022-05-17 07:14:17 -0300

    aula16

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,3 +1,16 @@
 let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(4)
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posoções`)
+console.loh(`O primeiro valor do vetor é ${num[0]}`)
 
-console.log(`Nosso valor é o ${num.length}`)
+for(var pos=0;pos<num.length;pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+for(var pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+num.indexOf(2)
